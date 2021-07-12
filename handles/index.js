69d930c5c75fb3db1df3c1c18d65ba3086eea0bb
Piyaphat Pinyo <remove158@gmail.com>
2021-07-12 22:38:40 +0700

    test1

--- handles/index.js
+++ handles/index.js
@@ -1,6 +1,7 @@
-const command = require('./command')
-const reaction = require('./reaction')
-const voice = require('./voice')
+const command = require("./command");
+const reaction = require("./reaction");
+const voice = require("./voice");
 exports.command = command;
-exports.reaction= reaction
-exports.voice = voice
+exports.reaction = reaction;
+exports.voice = voice;
+//aaaa
